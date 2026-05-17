2f95f0dfdcbb10d604806f67eb3db5f4ed41def2
Владислав Дядев <465824@niuitmo.ru>
2026-05-17 16:38:21 +0300

    fix: 🐛 script for running targets on MacOS

--- docker/run_ant_mac.sh
+++ docker/run_ant_mac.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 docker run --rm \
-  --platform linux/amd64 \
-  -v $(pwd):/project \
-  -w /project \
-  -v $(pwd)/.ant:/.ant \
-  -v $HOME/.ssh:/ssh \
+  --user "$(id -u):0" \
+  -v .:/project \
+  -v .ant:/.ant \
+  -v ../remote:/project/sandbox/remote \
   ghcr.io/itmo-fse-ld-2026/3-ant-environment:$1 \
-  ant -lib /ant/lib $2
+  ant $2
